8917950872609a562cb039717faa37b4fe20e38f
Robin THONI <robin@rthoni.com>
2021-08-15 10:25:40 +0200

    Removed `main()` args

--- janitor.py
+++ janitor.py
@@ -4,4 +4,4 @@ from pynvr2.janitor.main import main
 
 
 if __name__ == '__main__':
-    sys.exit(main(sys.argv))
+    sys.exit(main())
--- recorder.py
+++ recorder.py
@@ -4,4 +4,4 @@ from pynvr2.recorder.main import main
 
 
 if __name__ == '__main__':
-    sys.exit(main(sys.argv))
+    sys.exit(main())
