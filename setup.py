f8db96f145fd59ea91c0187c35ef39dc672dfc3d
Robin THONI <robin@rthoni.com>
2021-08-15 12:56:19 +0200

    Fixed supported Python versions

--- setup.py
+++ setup.py
@@ -54,12 +54,10 @@ pynvr2-janitor rotates video files following user-defined policies.
         # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
 
     keywords="CCTV NVR DVR camera cameras IP",
